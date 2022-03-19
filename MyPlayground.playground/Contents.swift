6c281276ea9853fc88c506a3f514c98afae68de4
Anas-0 <anas.almutawa1@gmail.com>
2022-03-19 11:02:32 +0300

    Done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct person {
+  
+
+    var name: String
+    var age: Int
+    var grade: Double
+    var attendence: Bool
+}
+var student1 = person(name: "anas", age: 16, grade: 90.0, attendence: true)
+
+var students = [student1,person(name: "ahmad", age: 30, grade: 99.0, attendence: false)]
